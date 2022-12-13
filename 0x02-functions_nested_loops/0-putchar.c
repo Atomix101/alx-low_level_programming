112af036fd3295d885b1bc1c6750bd06dc1a9ecc
Atomix101 <ahmed.shihabeldin101@gmail.com>
2022-12-13 03:53:21 -0800

    0-putchar.c3t

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "stdio.h"
 /**
- *
- *  * Print "_putchar" followed by a new line.
- *
- *   * Description: You are not allowed to include standard libraries.
- *
- *    * Return: 0
- *
- *     */
+ *main - Print "_putchar" followed by a new line.
+ *Description: You are not allowed to include standard libraries.
+ *Return: 0
+ */
 
-int main(void){
-	char myVar[] = "putchar";
+int main(void)
+{
+	char myVar[] = "_putchar";
 	int i = 0;
-	while (myVar[i] != '\0'){
+
+	while (myVar[i] != '\0')
+	{
 		putchar(myVar[i]);
 		i++;
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
