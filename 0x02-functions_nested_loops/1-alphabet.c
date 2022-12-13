9a93fc7518ec70dae3c594be7af3985b18d59aaf
Atomix101 <ahmed.shihabeldin101@gmail.com>
2022-12-13 05:56:49 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+include "main.h"
+/*
+ *print alpabets
+ *Description: use for loop to print small letters
+ *retutn:0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (char = 'a'; char = 'z'; char++)
+	{
+		_putchar(c)
+	}
+	_putchar(\n)
+}
