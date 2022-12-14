5c1230e6c6ea31b00e40fba4afae71fd6751f602
Atomix101 <ahmed.shihabeldin101@gmail.com>
2022-12-13 18:14:01 -0800

    9-times_table.c2t

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@ void times_table(void)
 	int x, y, z;
 
 	for (x = 0; x < 10; x++)
+	{
 		for (y = 0; y < 10; y++)
 		{
 			z = x * y;
@@ -30,5 +31,5 @@ void times_table(void)
 			}
 		}
 	_putchar('\n');
-}
+	}
 }
