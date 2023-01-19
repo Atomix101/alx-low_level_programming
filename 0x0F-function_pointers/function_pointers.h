dfb06c4552158218a3da055238c4853c8c5a67d2
Atomix101 <117734379+Atomix101@users.noreply.github.com>
2023-01-19 03:31:12 +0200

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,39 +1,6 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
-
-int _putchar(char c);
-
-void reset_to_98(int *n);
-
-void swap_int(int *a, int *b);
-
-int _strlen(char *s);
-
-void _puts(char *str);
-
-void print_rev(char *s);
-
-void rev_string(char *s);
-
-void puts2(char *str);
-
-void puts_half(char *str);
-
-void print_array(int *a, int n);
-
-char *_strcpy(char *dest, char *src);
-
-int _atoi(char *s);
-
-
-
-
-
-
-
-
-
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
