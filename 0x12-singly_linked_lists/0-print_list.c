3eef0eaed8a6e988b5ea2de2e14cd8badd479121
Atomix101 <ahmed.shihabeldin101@gmail.com>
2023-01-26 16:20:11 -0800

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - prints all list elements of list_t.
+ * @h: signly linked list
+ * Return: number of lidt elements.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nelem;
+
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(null)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
+}
